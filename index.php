4a56a94ca59f3d6966411c59c3b86f1e772c5320
Zebler <94908862+Zebler@users.noreply.github.com>
2024-05-09 23:46:13 +0700

    1 chút cart link nút login

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 <title>I D E A</title>
 </head>
 <body>
 <div id="warrper">
     <header id="header">
-      <?php include 'view/page/user/header.php' ?>
+      <?php require 'view/pages/user/index.php' ?>
     </header>
 </body>
 </html>
+  
